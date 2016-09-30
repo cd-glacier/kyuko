88a2b5320c09b3e2a914169902ebc243e3dfe9c0
Hyoga Yoshida <hyoga0216@gmail.com>
2016-09-30 17:40:49 +0900

    ...

--- go/model/model_test.go
+++ go/model/model_test.go
@@ -32,12 +32,6 @@ func TestInsert(t *testing.T) {
 	if err != nil {
 		t.Fatalf("insert に失敗\n%s", err)
 	}
-
-	result, err := db.DeleteWhereDayAndClassName("2016/09/26", "Insert Test")
-	affectedRows, _ := result.RowsAffected()
-	if err != nil || int(affectedRows) <= 0 {
-		t.Fatalf("deleteに失敗\n%s", err)
-	}
 }
 
 func TestSelectAll(t *testing.T) {
@@ -51,7 +45,6 @@ func TestSelectAll(t *testing.T) {
 	defer db.Close()
 
 	testData := KyukoData{Place: 1, Weekday: 1, Period: 1, Day: "2016/09/26", ClassName: "SelectAll Test", Instructor: "tsetMan", Reason: "darui"}
-
 	_, err = db.Insert(testData)
 	if err != nil {
 		t.Fatalf("insert に失敗: %s", err)
@@ -60,6 +53,24 @@ func TestSelectAll(t *testing.T) {
 	_, err = db.SelectAll()
 	if err != nil {
 		t.Fatalf("selectAll に失敗\n%s", err)
+	}
+
+}
+
+func TestDelete(t *testing.T) {
+	var db DB
+	var err error
+
+	err = db.Connect()
+	if err != nil {
+		t.Fatal("データベースに接続できません")
+	}
+	defer db.Close()
+
+	testData := KyukoData{Place: 1, Weekday: 1, Period: 1, Day: "2016/09/26", ClassName: "SelectAll Test", Instructor: "tsetMan", Reason: "darui"}
+	_, err = db.Insert(testData)
+	if err != nil {
+		t.Fatalf("insert に失敗: %s", err)
 	}
 
 	result, err := db.DeleteWhereDayAndClassName("2016/09/26", "SelectAll Test")
