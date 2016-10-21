1545783d6b2c49d723350d70441109aa428e0a64
Hyoga Yoshida <hyoga0216@gmail.com>
2016-10-21 14:15:22 +0900

    convert int to kanji

--- go/twitter/twitter.go
+++ go/twitter/twitter.go
@@ -45,6 +45,17 @@ func CreateLine(kyuko model.KyukoData) (string, error) {
 	return line, nil
 }
 
+// convert weekday(int) to weekday(KANJI)
+func ConvertWeekItos(weekday int) (string, error) {
+	weekKANJI := []string{"日", "月", "火", "水", "木", "金", "土"}
+
+	if weekday < 0 || weekday > 6 {
+		return "", errors.New("存在しない曜日が入力されています")
+	}
+
+	return weekKANJI[weekday], nil
+}
+
 // create tweet template
 // exsample
 //
@@ -54,9 +65,10 @@ func CreateLine(kyuko model.KyukoData) (string, error) {
 // ...
 //
 // in 140 characters
-func CreateContent(kyuko []model.KyukoDara) ([]string, error) {
+func CreateContent(kyuko []model.KyukoData) ([]string, error) {
 	var tws []string
 
+	// create lines
 	var lines []string
 	for _, v := range kyuko {
 		line, err := CreateLine(v)
@@ -66,12 +78,26 @@ func CreateContent(kyuko []model.KyukoDara) ([]string, error) {
 		lines = append(lines, line)
 	}
 
+	// convert weekday
 	weekday := kyuko[0].Weekday
-	stringWeekday := time.Weekday(weekday)
-	tw := "曜日の休講情報\n"
-	for i, v := range lines {
-
+	KANJIweekday, err := ConvertWeekItos(weekday)
+	if err != nil {
+		return tws, err
 	}
+
+	// create content
+	tw := KANJIweekday + "曜日の休講情報\n"
+	for _, line := range lines {
+		if len([]rune(line)) < 140 {
+			tw = tw + line
+		} else {
+			tws = append(tws, tw)
+			tw = KANJIweekday + "曜日の休講情報\n"
+			tw = tw + line
+		}
+	}
+
+	return tws, err
 }
 
 // tweet argment
