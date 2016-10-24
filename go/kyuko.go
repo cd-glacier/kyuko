f77c47a968e19275e508d717979fb8bd5405b77b
Hyoga Yoshida <hyoga0216@gmail.com>
2016-10-24 18:44:13 +0900

    現在の時間取得してtweet

--- go/kyuko.go
+++ go/kyuko.go
@@ -1,34 +1,32 @@
 package kyuko
 
 import (
-	"os"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
+	goTwitter "github.com/dghubble/go-twitter/twitter"
 	"github.com/g-hyoga/kyuko/go/model"
 	"github.com/g-hyoga/kyuko/go/scrape"
 	"github.com/g-hyoga/kyuko/go/twitter"
 )
 
-var (
-	T_CONSUMER_KEY        = os.Getenv("T_CONSUMER_KEY")
-	T_CONSUMER_SECRET     = os.Getenv("T_CONSUMER_SECRET")
-	T_ACCESS_TOKEN        = os.Getenv("T_ACCESS_TOKEN")
-	T_ACCESS_TOKEN_SECRET = os.Getenv("T_ACCESS_TOKEN_SECRET")
-
-	I_CONSUMER_KEY        = os.Getenv("I_CONSUMER_KEY")
-	I_CONSUMER_SECRET     = os.Getenv("I_CONSUMER_SECRET")
-	I_ACCESS_TOKEN        = os.Getenv("I_ACCESS_TOKEN")
-	I_ACCESS_TOKEN_SECRET = os.Getenv("I_ACCESS_TOKEN_SECRET")
-)
-
 // scrapingを実行してデータベースに保存する
 // その後twitterに投稿
-func Exec() error {
+func Exec(place int, client *goTwitter.Client) error {
 	var err error
+
+	//今日の日付
+	weekday := int(time.Now().Weekday())
+	//今の時間
+	nowTime := time.Now().Hour()
+	// 18:00超えてたら次の日の情報にする
+	if nowTime >= 18 {
+		weekday += 1
+	}
 
 	//第一引数:校地
 	//第二引数:曜日
-	url, err := scrape.SetUrl(2, 1)
+	url, err := scrape.SetUrl(place, weekday)
 	if err != nil {
 		return err
 	}
@@ -68,12 +66,8 @@ func Exec() error {
 		return err
 	}
 
-	//iClient := NewTwitterClient(I_CONSUMER_KEY, I_CONSUMER_SECRET, I_ACCESS_TOKEN, I_ACCESS_TOKEN_SECRET)
-	tClient := twitter.NewTwitterClient(T_CONSUMER_KEY, T_CONSUMER_SECRET, T_ACCESS_TOKEN, T_ACCESS_TOKEN_SECRET)
-
 	for _, tw := range tws {
-		//twitter.Update(iClient, tw)
-		twitter.Update(tClient, tw)
+		twitter.Update(client, tw)
 	}
 
 	return nil
